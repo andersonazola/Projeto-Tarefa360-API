d19995162f57848d9665ed17e4d767578917aad3
ZettiCavalcanti <luiz.fscavalcanti@outlook.com>
2026-08-10 23:50:32 -0300

    chore - repositorio: registra DbSet de Projeto no Projeto360Contexto

--- Projeto360.Repositorio/Contexto/Projeto360Contexto.cs
+++ Projeto360.Repositorio/Contexto/Projeto360Contexto.cs
@@ -7,6 +7,7 @@ public class Projeto360Contexto : DbContext
 {
     private readonly DbContextOptions _options;
     public DbSet<Usuario> Usuarios { get; set; }
+    public DbSet<Projeto> Projetos { get; set; }
     
     // public Projeto360Contexto() { }
     public Projeto360Contexto(DbContextOptions options) : base(options) { }
@@ -15,10 +16,8 @@ public class Projeto360Contexto : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfiguration(new UsuarioConfiguracoes());
+        modelBuilder.ApplyConfiguration(new ProjetoConfiguracoes());
         
     }
-
-
-
 
 }
