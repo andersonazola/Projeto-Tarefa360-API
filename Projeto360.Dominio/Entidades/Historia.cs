8c9b857144d7da533bdc60f6eeef1d158de0f232
Sara Pereira Gonçalves <contato.saragoncalves@outlook.com>
2026-08-12 20:38:38 -0300

    feat - dominio: adiciona entidade História

---
+++ Projeto360.Dominio/Entidades/Historia.cs
@@ -0,0 +1,9 @@
+namespace Projeto360.Entidades;
+
+public class Historia
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    // public Projeto Projeto { get; set; }
+    public string Descricao { get; set; }
+}
