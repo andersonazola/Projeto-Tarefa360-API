2bd6f03957e8ef8f0cb0f655bd90dd425fb9e409
ZettiCavalcanti <luiz.fscavalcanti@outlook.com>
2026-08-14 22:33:52 -0300

    fix - historia.cs: correção nas propriedade

--- Projeto360.Dominio/Entidades/Historia.cs
+++ Projeto360.Dominio/Entidades/Historia.cs
@@ -1,9 +1,18 @@
+using Projeto360.Dominio.Entidades;
+
 namespace Projeto360.Entidades;
 
 public class Historia
 {
     public int Id { get; set; }
     public string Nome { get; set; }
-    // public Projeto Projeto { get; set; }
+    public Projeto Projeto { get; set; }
+    public int ProjetoId { get; set; }
     public string Descricao { get; set; }
+    public bool Ativo { get; set; }
+
+    public Historia()
+    {
+        Ativo = true;
+    }
 }
